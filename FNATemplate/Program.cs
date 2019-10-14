d058cf361063b9cb8d5121af47d0cd4cb5241b66
Josiah P <vasti009@gmail.com>
2019-10-14 14:11:16 -0300

    Finished template

---
+++ FNATemplate/Program.cs
@@ -0,0 +1,16 @@
+﻿using FNATemplate.Game;
+using System;
+
+namespace FNATemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var game = new TemplateClient())
+            {
+                game.Run();
+            }
+        }
+    }
+}
